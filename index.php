fa5ba652c5c5d9382db4fa277513eaef23515dfd
sophiamziou <sofiamziou@gmail.com>
2023-02-08 00:36:13 +0100

    exercise done

--- index.php
+++ index.php
@@ -1,4 +1,43 @@
 <?php
+
+class Movie{
+    public $title;
+    public $language;
+    public $genre;
+    public $year;
+    public $discount;
+
+    function __construct($_title,$_language,$_genre,$_year){
+        $this->title = $_title;
+        $this->language = $_language;
+        $this->genre = $_genre;
+        $this->year = $_year;
+    }
+
+    public function setDiscount($year){
+        if ($year < 2000){
+            $this->discount = 50;
+        }
+        else {
+            $this->discount = 10;
+        }
+    }
+    public function getDiscount(){
+        return $this->discount;
+    }
+};
+
+$movie_1 = new Movie('Avatar','Inglese','Fantasy','2022');
+$movie_2 = new Movie('Titanic','Italiano','Romantico','1997');
+$movie_3 = new Movie('Jurassic World','Inglese','Sci-fi','2015');
+
+$array_movie = 
+[
+    $movie_1,
+    $movie_2,
+    $movie_3
+]
+
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +52,33 @@
 </head>
 
 <body>
+    <h1>LISTA FILM</h1>
+    <div>I film usciti prima del 2000 hanno lo sconto del 50%,quelli dopo il 2000 del 10%</div>
+    <div>
+        <?php
+            foreach ($array_movie as $key) {
+                ?>
+        <h2>
+            <?php
+                echo $key->title;
+            ?>
+        </h2>
+        <p>
+            <?php
+                echo $key->language;
+                echo "<br>".$key->genre;
+                echo "<br>".$key->year;
+            ?>
+        </p>
+        <span>
+            <?php
+                $movie_year = $key->year;
+                $key->setDiscount($movie_year);
+                echo "Sconto:". $key->getDiscount()."%";
+            ?>
+        </span>
+        <?php }?>
+    </div>
 </body>
 
 </html>
